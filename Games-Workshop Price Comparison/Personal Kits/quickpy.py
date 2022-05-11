9eff5590a0d0146a3b3bf3ab1f6f9f7e2ddba042
Rod Fitzgerald <RoderickAus@gmail.com>
2022-05-11 18:30:03 +1000

    removed pointless string splicing.

--- Games-Workshop Price Comparison/Personal Kits/quickpy.py
+++
@@ -1,8 +0,0 @@
-stringA = "Killdakka Warband (Orks), Shieldbreaker (Marines), Eldritch Omens x2 (Chaos & Eldar), Lynx (Eldar), Hornet x2 (Eldar), Wraithknight x3 (Eldar), Wolves combat patrol, Eldar combat patrol, Skitarii (AdMech), Chaos Marines (about half done), Meganobz (Orks), Boyz (Orks), Primaris Rievers (Marines), Maugen Ra (Eldar), Guardians (Eldar), Dark Reapers (Eldar), Venerable Dreadnought (Marines), ThunderCav (Marines), Chaos Bikers"
-stringB = stringA.split(',')
-c = 1
-for a in stringB:
-    c = c + 1
-    print(str(c) + '.')
-    #print(a)
-    #print("\n")
